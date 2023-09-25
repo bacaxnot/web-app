5dfed85a54e116be7f080cb87d7decfad54d6b99
bacaxnot <cala.js@icloud.com>
2023-09-25 16:59:17 -0500

    experiment on deploy

--- src/hooks/use-admin-auth.ts
+++ src/hooks/use-admin-auth.ts
@@ -3,6 +3,10 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 export default function useAdminAuth() {
   const supabase = createClientComponentClient();
 
+  console.log(
+    "using admint auth to redirect to:",
+    `${location.origin}/api/auth/callback`,
+  );
   const authorize = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "github",
